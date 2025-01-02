00d4cd6578389b0711267c7ccf24181f1d8338cb
Åsa Stabler <asa.stabler@gmail.com>
2025-01-02 16:53:07 +0100

    Built IContactRepository and ContactRepository

---
+++ Business/Repositories/ContactRepository.cs
@@ -0,0 +1,45 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace Business.Repositories;
+
+public class ContactRepository(IFileService fileService) : IContactRepository
+{
+    private readonly IFileService _fileService = fileService;
+
+    //Method saves the whole contact list, i.e. contacts, to file
+    public bool SaveContacts(List<Contact> contacts)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(contacts);
+            _fileService.SaveContentToFile(json);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    //Method reads the whole contact list, i.e. contacts, from file
+    public List<Contact>? GetContacts()
+    {
+        try
+        {
+            var json = _fileService.GetContentFromFile();
+            var contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+            return contacts;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return [];
+        }
+    }
+
+
+}
--- Business/Services/ContactService.cs
+++ Business/Services/ContactService.cs
@@ -1,28 +1,60 @@
 ﻿using Business.Helpers;
 using Business.Interfaces;
 using Business.Models;
+using System.Diagnostics;
 using System.Text.Json;
 
 namespace Business.Services;
 
-public class ContactService(IFileService fileService) : IContactService
+public class ContactService(IContactRepository contactRepository) : IContactService
 {
-    private readonly IFileService _fileService = fileService;
+    //private readonly IFileService _fileService = fileService;
+    private readonly IContactRepository _contactRepository = contactRepository;
     private List<Contact> _contacts = [];
 
     public bool CreateContact(Contact contact)
     {
-        contact.Id = IdGenerator.GenerateUniqueId();
+        try
+        {
+            contact.Id = IdGenerator.GenerateUniqueId();
 
-        _contacts.Add(contact);
+            _contacts.Add(contact);
 
-        var json = JsonSerializer.Serialize(contact);
-        _fileService.SaveContentToFile(json);
-        return true;
+            //var json = JsonSerializer.Serialize(contact);
+            //_fileService.SaveContentToFile(json);
+            //return true;
+
+            var result = _contactRepository.SaveContacts(_contacts);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public IEnumerable<Contact> GetAllContacts()
     {
-        throw new NotImplementedException();
+         _contacts = _contactRepository.GetContacts()!;
+         return _contacts;
     }
+
+    /*** NOT necessary to implement try-catch here - it gives the same result when returning _contacts från contactRepository.GetContacts(). ***/
+    /*** ...it returns an empty list [] if something goes wrong ***/
+    /*
+    public IEnumerable<Contact> GetAllContacts()
+    {
+        try
+        {
+            _contacts = _contactRepository.GetContacts()!;
+            return _contacts;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return [];
+        }
+    }
+    */
 }
